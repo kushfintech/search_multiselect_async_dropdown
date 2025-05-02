a7dc40bbcdea4471cb9ec4145f95539f0ae6dec4
kushfintech-luniva <luniva.singh@kushfintech.com>
2025-05-02 10:40:07 +0545

    chip ui fixed

--- lib/src/multi_dropdown.dart
+++ lib/src/multi_dropdown.dart
@@ -695,13 +695,12 @@ class _MultiDropdownState<T extends Object> extends State<MultiDropdown<T>> {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Expanded(
-            child: Text(
-              option.label,
-              style: chipDecoration.labelStyle,
-
-              overflow: TextOverflow.clip,
-            ),
+          Text(
+            option.label,
+            style: chipDecoration.labelStyle,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            softWrap: false,
           ),
           const SizedBox(width: 4),
           InkWell(
@@ -710,13 +709,8 @@ class _MultiDropdownState<T extends Object> extends State<MultiDropdown<T>> {
                 (element) => element.label == option.label,
               );
             },
-            child: SizedBox(
-              width: 16,
-              height: 16,
-              child:
-                  chipDecoration.deleteIcon ??
-                  const Icon(Icons.close, size: 16),
-            ),
+            child:
+                chipDecoration.deleteIcon ?? const Icon(Icons.close, size: 13),
           ),
         ],
       ),
